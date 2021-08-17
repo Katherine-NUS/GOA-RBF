aff05a8867b3a1d0be7caea925c650f40ce2d6a4
Xiaoqian <zhouxiaoqiankath@gmail.com>
2021-08-17 21:45:52 +0800

    Added DYCORS and GOMORS example

---
+++ examples/GOMORS.py
@@ -0,0 +1,9 @@
+from GOMORS2.MOoptim import MOoptimize
+from GOMORS2.test_problems import DTLZ4
+
+data = DTLZ4(nobj=2)
+# Serial GOMORS as the default optimizor
+MOoptimize(data)
+
+# Parallel GOMORS
+MOoptimize(data, run='asynchronous')
